ef52bb0f8cf9158f9d7f45488ac55bf44fd2fcf6
Bianca Belei <biebianca99@gmail.com>
2023-06-06 17:24:58 +0300

    update

--- components/Charts/ChartComponent.jsx
+++ components/Charts/ChartComponent.jsx
@@ -1,36 +1,124 @@
 import React, { useEffect, useState, useRef } from "react";
-import Chart from "chart.js";
-import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  LineElement,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+} from "chart.js";
+ChartJS.register(
+  Title,
+  Tooltip,
+  LineElement,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement
+);
+import html2canvas from "html2canvas";
+import { saveAs } from "file-saver";
+import { toJpeg } from "dom-to-image-more";
 
-export default function ChartComponent({ country, indicator }) {
-  //   const [data, setData] = useState(null);
-  //   useEffect(() => {
-  //     const indicatorData = country.ani
-  //       ? country.ani.reduce((acc, curr) => {
-  //           const foundIndicator = curr.indicatori.find(
-  //             (indicator) => indicator.nume === indicator
-  //           );
-  //           if (indicator) {
-  //             acc.push({ year: curr.an, value: indicator.valoare });
-  //           }
-  //           return acc;
-  //         }, [])
-  //       : [];
-  //     setData({
-  //       labels: indicatorData.map((data) => data.year),
-  //       datasets: [
-  //         {
-  //           label: indicator,
-  //           data: indicatorData.map((data) => data.value),
-  //           borderColor: "rgba(75,192,192,1)",
-  //           borderWidth: 2,
-  //           pointBackgroundColor: "rgba(75,192,192,1)",
-  //           pointBorderColor: "#fff",
-  //           pointHoverBackgroundColor: "#fff",
-  //           pointHoverBorderColor: "rgba(75,192,192,1)",
-  //         },
-  //       ],
-  //     });
-  //   }, [country, indicator]);
-  //   return data ? <Line data={data} /> : <p>Loading...</p>;
+import { Line } from "react-chartjs-2";
+import styles from "./chartComponent.module.css";
+
+export default function ChartComponent({ country, indicator, data }) {
+  if (!data || data.length === 0) {
+    return <div>No data available for the selected indicator.</div>;
+  }
+
+  const years = data.map((year) => year.an);
+  const valori = data.map(
+    (year) => year.indicatori.find((i) => i.nume === indicator)?.valoare
+  );
+
+  const chartData = {
+    labels: years,
+    datasets: [
+      {
+        label: `${country} - ${indicator}`,
+        data: valori,
+        backgroundColor: "#ff4c30",
+        borderColor: "#ff4c30",
+        borderWidth: 2,
+        pointRadius: 0,
+      },
+    ],
+  };
+  const chartOptions = {
+    plugins: {
+      title: {
+        display: true,
+        text: `${indicator.toUpperCase()}`,
+        font: {
+          size: 20,
+          weight: "bold",
+        },
+        color: "white",
+        padding: {
+          bottom: 30,
+          top: 10,
+        },
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        grid: {
+          color: "rgba(0, 0, 0, 0.1)",
+        },
+        ticks: {
+          font: {
+            size: 12,
+          },
+        },
+      },
+    },
+    layout: {
+      padding: {
+        right: 1, // Ajustați acest număr pentru a stabili spațiul din dreapta al graficului
+        top: 20, // Adăugați spațiul dorit între titlu și grafic (valoarea poate fi ajustată)
+        // bottom: 10,
+      },
+    },
+  };
+  const chartRef = useRef(null);
+  const exportChartAsJpg = () => {
+    console.log("Exporting chart as JPG");
+    const chartElement = chartRef.current?.chartInstance?.canvas;
+    console.log(chartElement);
+    if (chartElement) {
+      toJpeg(chartElement, { quality: 0.95 })
+        .then(function (dataUrl) {
+          var link = document.createElement("a");
+          link.download = "chart.jpg";
+          link.href = dataUrl;
+          link.click();
+        })
+        .catch(function (error) {
+          console.error("Error exporting chart as JPG:", error);
+        });
+    }
+  };
+
+  return (
+    <div className={styles.table}>
+      <Line
+        ref={chartRef}
+        data={chartData}
+        options={chartOptions}
+        height={250}
+        width={500}
+        className={styles.ChartComponent}
+      />
+      <button onClick={exportChartAsJpg}>Export as JPG</button>
+    </div>
+  );
 }
--- components/Indicators/IndicatorsBar.jsx
+++ components/Indicators/IndicatorsBar.jsx
@@ -10,7 +10,135 @@ import date from "../../public/date.json";
 import ChartComponent from "../Charts/ChartComponent";
 
 export default function IndicatorsBar({ country }) {
-  const [selectedIndicators, setSelectedIndicators] = useState([]);
+  //   const [selectedIndicators, setSelectedIndicators] = useState([]);
+  //   const [previousIndicator, setPreviousIndicator] = useState(null);
+  //   const indicators = country.ani.length > 0 ? country.ani[0].indicatori : [];
+
+  //   const handleCheckboxChange = (event) => {
+  //     const target = event.target;
+  //     const indicatorName = target.name;
+
+  //     //   if (target.checked) {
+  //     //     setSelectedIndicators([...selectedIndicators, indicatorName]);
+  //     //   } else {
+  //     //     setSelectedIndicators(
+  //     //       selectedIndicators.filter((indicator) => indicator !== indicatorName)
+  //     //     );
+  //     //   }
+  //     // };
+  //     if (target.checked) {
+  //       setSelectedIndicators((prevSelectedIndicators) => [
+  //         ...prevSelectedIndicators,
+  //         indicatorName,
+  //       ]);
+  //     } else {
+  //       setSelectedIndicators((prevSelectedIndicators) =>
+  //         prevSelectedIndicators.filter(
+  //           (indicator) => indicator !== indicatorName
+  //         )
+  //       );
+  //     }
+  //   };
+
+  //   const handleIndicatorClick = (indicator) => {
+  //     // setSelectedIndicators(indicator);
+  //     setPreviousIndicator(selectedIndicators[0]); // Salvați indicatorul anterior în starea previousIndicator
+  //     setSelectedIndicators([indicator]);
+  //   };
+  //   // useEffect(() => {
+  //   //   if (previousIndicator) {
+  //   //     setSelectedIndicators((prevSelectedIndicators) =>
+  //   //       prevSelectedIndicators.filter(
+  //   //         (indicator) => indicator === previousIndicator
+  //   //       )
+  //   //     );
+  //   //   }
+  //   // }, [previousIndicator]); // Efectul se activează atunci când indicatorul anterior se schimbă
+  //   useEffect(() => {
+  //     if (previousIndicator && selectedIndicators.length > 1) {
+  //       setSelectedIndicators([
+  //         selectedIndicators[selectedIndicators.length - 1],
+  //       ]);
+  //     }
+  //   }, [previousIndicator, selectedIndicators]);
+
+  //   return (
+  //     <div>
+  //       <div className={styles.title}>
+  //         <h2>{country.nume}</h2>
+  //       </div>
+  //       {indicators.length > 0 && (
+  //         <div className={styles.display}>
+  //           {indicators.map((indicator) => (
+  //             <div key={indicator.nume}>
+  //               <input
+  //                 type="checkbox"
+  //                 name={indicator.nume}
+  //                 id={indicator.nume}
+  //                 onChange={handleCheckboxChange}
+  //                 checked={selectedIndicators.includes(indicator.nume)}
+  //                 className={styles["checkbox-input"]}
+  //               />
+  //               <label
+  //                 htmlFor={indicator.nume}
+  //                 className={styles["checkbox-label"]}
+  //               >
+  //                 <h3 className={styles.hover}>{indicator.nume}</h3>
+  //               </label>
+  //             </div>
+  //           ))}
+  //         </div>
+  //       )}
+  //       {/* {selectedIndicators.length > 0 && (
+  //           <div className={styles.divIndicator}>
+  //             {selectedIndicators.map((indicatorName) => {
+  //               const selectedIndicator = country.ani
+  //                 .map((item) => ({
+  //                   an: item.an,
+  //                   indicatori: item.indicatori,
+  //                 }))
+  //                 .filter((item) =>
+  //                   item.indicatori.some((i) => i.nume === indicatorName)
+  //                 );
+
+  //               return (
+  //                 <ChartComponent
+  //                   key={indicatorName}
+  //                   country={country.nume}
+  //                   indicator={indicatorName}
+  //                   data={selectedIndicator}
+  //                 />
+  //               );
+  //             })}
+  //           </div>
+  //         )} */}
+  //       {selectedIndicators.length > 0 && (
+  //         <div className={styles.divIndicator}>
+  //           {selectedIndicators.map((indicatorName) => {
+  //             const selectedIndicator = country.ani
+  //               .map((item) => ({
+  //                 an: item.an,
+  //                 indicatori: item.indicatori,
+  //               }))
+  //               .filter((item) =>
+  //                 item.indicatori.some((i) => i.nume === indicatorName)
+  //               );
+
+  //             return (
+  //               <ChartComponent
+  //                 key={indicatorName}
+  //                 country={country.nume}
+  //                 indicator={indicatorName}
+  //                 data={selectedIndicator}
+  //               />
+  //             );
+  //           })}
+  //         </div>
+  //       )}
+  //     </div>
+  //   );
+  // }
+  const [selectedIndicator, setSelectedIndicator] = useState(null);
   const indicators = country.ani.length > 0 ? country.ani[0].indicatori : [];
 
   const handleCheckboxChange = (event) => {
@@ -18,18 +146,24 @@ export default function IndicatorsBar({ country }) {
     const indicatorName = target.name;
 
     if (target.checked) {
-      setSelectedIndicators([...selectedIndicators, indicatorName]);
-    } else {
-      setSelectedIndicators(
-        selectedIndicators.filter((indicator) => indicator !== indicatorName)
-      );
+      setSelectedIndicator(indicatorName);
     }
   };
+
+  const handleIndicatorClick = (indicator) => {
+    setSelectedIndicator(indicator);
+  };
+
+  useEffect(() => {
+    if (selectedIndicator === null && indicators.length > 0) {
+      setSelectedIndicator(indicators[0].nume);
+    }
+  }, [indicators, selectedIndicator]);
 
   return (
     <div>
       <div className={styles.title}>
-        <h2>{country.nume} </h2>
+        <h2>{country.nume}</h2>
       </div>
       {indicators.length > 0 && (
         <div className={styles.display}>
@@ -40,102 +174,37 @@ export default function IndicatorsBar({ country }) {
                 name={indicator.nume}
                 id={indicator.nume}
                 onChange={handleCheckboxChange}
-                checked={selectedIndicators.includes(indicator.nume)}
+                checked={selectedIndicator === indicator.nume}
                 className={styles["checkbox-input"]}
               />
               <label
                 htmlFor={indicator.nume}
                 className={styles["checkbox-label"]}
               >
-                <h3 className={styles.hover}>{indicator.nume}</h3>
+                <h3
+                  className={styles.hover}
+                  onClick={() => handleIndicatorClick(indicator.nume)}
+                >
+                  {indicator.nume}
+                </h3>
               </label>
             </div>
           ))}
         </div>
       )}
-      {selectedIndicators.length > 0 && (
+      {selectedIndicator && (
         <div className={styles.divIndicator}>
-          {selectedIndicators.map((indicatorName) => {
-            const selectedIndicator = indicators.find(
-              (indicator) => indicator.nume === indicatorName
-            );
-            return (
-              <ChartComponent
-                key={selectedIndicator.nume}
-                country={country.nume}
-                indicator={selectedIndicator.nume}
-                value={selectedIndicator.valoare}
-              />
-            );
-          })}
+          <ChartComponent
+            key={selectedIndicator}
+            country={country.nume}
+            indicator={selectedIndicator}
+            data={country.ani.map((item) => ({
+              an: item.an,
+              indicatori: item.indicatori,
+            }))}
+          />
         </div>
       )}
     </div>
   );
-
-  // const [indicators, setIndicators] = useState([]);
-  // const [selectedIndicators, setSelectedIndicators] = useState(null);
-
-  // useEffect(() => {
-  //   const { ani } = country;
-  //   if (ani.length > 0) {
-  //     setIndicators(ani[0].indicatori); // setam indicatorii primului an
-  //   }
-  // }, [country]);
-
-  // // const handleIndicatorClick = (indicator) => {
-  // //   setSelectedIndicator(indicator);
-  // // };
-  // const handleCheckboxChange = (event) => {
-  //   const target = event.target;
-  //   const indicatorName = target.name;
-
-  //   if (target.checked) {
-  //     setSelectedIndicators([...selectedIndicators, indicatorName]);
-  //   } else {
-  //     setSelectedIndicators(selectedIndicators.filter((indicator) => indicator !== indicatorName));
-  //   }
-  // };
-
-  // return (
-  //   <div>
-  //     {" "}
-  //     <div className={styles.title}>
-  //       <h2>{country.nume} </h2>
-  //     </div>
-  //     {indicators.length > 0 && (
-  //       <div className={styles.display}>
-  //         {indicators.map((indicator) => (
-  //           <div key={indicator.nume}>
-  //             <input
-  //               type="radio"
-  //               name="dark"
-  //               id="dark"
-  //               value="dark"
-  //               onChange={onChange}
-  //               checked={true}
-  //             />
-  //             <h3
-  //               key={indicator.nume}
-  //               onClick={() => handleIndicatorClick(indicator)}
-  //               className={styles.hover}
-  //             >
-  //               {indicator.nume}
-  //             </h3>
-  //           </div>
-  //         ))}
-  //       </div>
-  //     )}
-  //     {selectedIndicator && (
-  //       <div>
-  //         <ChartComponent
-  //           country={country.nume}
-  //           //    year={year.an}
-  //           indicator={selectedIndicator.nume}
-  //           value={selectedIndicator.valoare}
-  //         />
-  //       </div>
-  //     )}
-  //   </div>
-  // );
 }
